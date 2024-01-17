711fcba4769a339541d0a5ae761b19a952366b9d
Jiajie Chen <c@jia.je>
2024-01-18 01:07:26 +0800

    Split qt patches

---
+++ qt6-webengine/split.py
@@ -0,0 +1,34 @@
+version = "6.5.1"
+with open(f"qt6-{version}.diff", "r") as f:
+    cur = []
+    patches = []
+    for line in f:
+        if line.startswith("diff "):
+            if len(cur) > 0:
+                patches.append(cur)
+            cur = []
+        cur.append(line)
+    if len(cur) > 0:
+        patches.append(cur)
+
+    classes = {"loongarch64": []}
+    for patch in patches:
+        path = patch[1].split(" ")[1].split("\t")[0][2:]
+
+        # classify
+        if path.startswith("qtwebengine/src/3rdparty/chromium/third_party/ffmpeg/"):
+            clazz = "loongarch64-ffmpeg"
+        elif path.startswith("qtwebengine/src/3rdparty/chromium/sandbox/"):
+            clazz = "loongarch64-sandbox"
+        else:
+            clazz = "loongarch64"
+
+        if clazz not in classes:
+            classes[clazz] = []
+        classes[clazz].append(patch)
+        print(path)
+
+    for clazz in classes:
+        with open(f"qt6-{version}.{clazz}.diff", "w") as f:
+            for patch in classes[clazz]:
+                print("".join(patch), end="", file=f)
