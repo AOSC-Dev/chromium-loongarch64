f8600c93b7cb2480b1b1ce941f1d6013328690bf
Jiajie Chen <c@jia.je>
2024-02-22 12:33:39 +0800

    Add comments on statx

--- chromium/split.py
+++ chromium/split.py
@@ -113,24 +113,6 @@ with open(f"chromium-{version}.diff", "r") as f:
             "third_party/libvpx/source/config/linux/loongarch/vp8_rtcd.h",
         ]:
             clazz = "loongarch64-clang-no-lsx"
-        elif path in [
-            "third_party/webrtc/rtc_base/string_encode.h",
-            "third_party/webrtc/api/stats/rtc_stats.h",
-            "third_party/swiftshader/third_party/llvm-16.0/llvm/lib/Transforms/IPO/AttributorAttributes.cpp",
-            "third_party/ruy/src/ruy/profiler/instrumentation.h",
-            "third_party/ruy/src/ruy/profiler/instrumentation.cc",
-            "third_party/protobuf/src/google/protobuf/repeated_ptr_field.h",
-            "third_party/perfetto/include/perfetto/base/status.h",
-            "third_party/material_color_utilities/src/cpp/palettes/tones.cc",
-            "third_party/blink/renderer/core/typed_arrays/dom_typed_array.h",
-            "third_party/blink/renderer/core/typed_arrays/dom_typed_array.cc",
-            "third_party/blink/renderer/core/frame/local_frame_client.h",
-            "third_party/blink/renderer/core/frame/local_frame.h",
-            "third_party/blink/renderer/bindings/core/v8/native_value_traits_buffer_sources.cc",
-            "third_party/blink/public/web/web_local_frame_client.h",
-            "third_party/blink/common/interest_group/auction_config_mojom_traits.cc",
-        ]:
-            clazz = "gcc"
         elif path.startswith("third_party/swiftshader/"):
             clazz = "loongarch64-swiftshader"
         elif path.startswith("sandbox/"):
