ed6922054b67ab1d541047691277a172ae988087
Jiajie Chen <c@jia.je>
2024-03-21 00:59:48 +0800

    Regenerate ffmpeg config files

--- chromium/gen.sh
+++ chromium/gen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd ../../chromium-123.0.6312.58
+pushd ../../chromium-123.0.6312.58
 diff '--color=auto' -p -X ../chromium-loongarch64/chromium/exclude -N -u -r a b > ../chromium-loongarch64/chromium/chromium-123.0.6312.58.diff
+popd
 bash strip-datetime.sh
