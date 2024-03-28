5d505600982cdbcbf08a0631e89f7cbd42ae34ae
Jiajie Chen <c@jia.je>
2024-03-28 14:53:06 +0800

    Rebase qt6 patches on 6.6.3

--- qt6-webengine/gen.sh
+++ qt6-webengine/gen.sh
@@ -1,3 +1,5 @@
 CWD=$PWD
-cd ../../qt-6.6.2
-diff '--color=auto' -p -X ../chromium-loongarch64/qt6-webengine/exclude -N -u -r a b > $CWD/qt6-6.6.2.diff
+pushd ../../qt-6.6.3
+diff '--color=auto' -p -X ../chromium-loongarch64/qt6-webengine/exclude -N -u -r a b > $CWD/qt6-6.6.3.diff
+popd
+bash ../chromium/strip-datetime.sh
--- qt6-webengine/split.py
+++ qt6-webengine/split.py
@@ -1,7 +1,7 @@
-version = "6.6.2"
+version = "6.6.3"
+patches = []
 with open(f"qt6-{version}.diff", "r") as f:
     cur = []
-    patches = []
     for line in f:
         if line.startswith("diff "):
             if len(cur) > 0:
@@ -11,30 +11,34 @@ with open(f"qt6-{version}.diff", "r") as f:
     if len(cur) > 0:
         patches.append(cur)
 
-    classes = {"loongarch64": []}
-    for patch in patches:
-        path = patch[1].split(" ")[1].split("\t")[0][2:]
+classes = {"loongarch64": []}
+for patch in sorted(patches):
+    path = patch[1].split(" ")[1].split("\t")[0][2:]
 
-        # classify
-        if path.startswith("qtwebengine/src/3rdparty/chromium/third_party/ffmpeg/"):
-            clazz = "loongarch64-ffmpeg"
-        elif path.startswith("qtwebengine/src/3rdparty/chromium/sandbox/"):
-            clazz = "loongarch64-sandbox"
-        elif path.startswith("qtwebengine/src/3rdparty/chromium/third_party/crashpad/"):
-            clazz = "loongarch64-crashpad"
-        elif path.startswith("qtwebengine/src/3rdparty/chromium/third_party/devtools-frontend/"):
-            clazz = "rollup"
-        elif path.startswith("qtwebengine/src/3rdparty/chromium/v8/src/regexp/"):
-            clazz = "loongarch64-regexp-backport"
-        else:
-            clazz = "loongarch64"
+    # classify
+    if path.startswith("qtwebengine/src/3rdparty/chromium/third_party/ffmpeg/"):
+        clazz = "loongarch64-ffmpeg"
+    elif path.startswith("qtwebengine/src/3rdparty/chromium/sandbox/"):
+        clazz = "loongarch64-sandbox"
+    elif path.startswith("qtwebengine/src/3rdparty/chromium/third_party/crashpad/"):
+        clazz = "loongarch64-crashpad"
+    elif path.startswith("qtwebengine/src/3rdparty/chromium/third_party/devtools-frontend/"):
+        clazz = "rollup"
+    elif path.startswith("qtwebengine/src/3rdparty/chromium/v8/src/regexp/"):
+        clazz = "loongarch64-regexp-backport"
+    else:
+        clazz = "loongarch64"
 
-        if clazz not in classes:
-            classes[clazz] = []
-        classes[clazz].append(patch)
-        print(path)
+    if clazz not in classes:
+        classes[clazz] = []
+    classes[clazz].append(patch)
+    print(path)
 
-    for clazz in classes:
-        with open(f"qt6-{version}.{clazz}.diff", "w") as f:
-            for patch in classes[clazz]:
-                print("".join(patch), end="", file=f)
+for clazz in classes:
+    with open(f"qt6-{version}.{clazz}.diff", "w") as f:
+        for patch in classes[clazz]:
+            print("".join(patch), end="", file=f)
+
+with open(f"qt6-{version}.diff", "w") as f:
+    for patch in sorted(patches):
+        print("".join(patch), end="", file=f)
