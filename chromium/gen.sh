3e7b217e27e7161a5673818d21c1c3977b34e52c
Jiajie Chen <c@jia.je>
2024-05-16 23:03:28 -0700

    Initial chromium 125 patches

--- chromium/gen.sh
+++ chromium/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pushd ../../chromium-124.0.6367.118
-diff '--color=auto' -p -X ../chromium-loongarch64/chromium/exclude -N -u -r a b > ../chromium-loongarch64/chromium/chromium-124.0.6367.118.diff
+pushd ../../chromium-125.0.6422.60
+diff '--color=auto' -p -X ../chromium-loongarch64/chromium/exclude -N -u -r a b > ../chromium-loongarch64/chromium/chromium-125.0.6422.60.diff
 popd
 bash strip-datetime.sh
--- chromium/split.py
+++ chromium/split.py
@@ -1,4 +1,4 @@
-version = "124.0.6367.118"
+version = "125.0.6422.60"
 patches = []
 with open(f"chromium-{version}.diff", "r") as f:
     cur = []
@@ -17,34 +17,9 @@ for patch in sorted(patches):
 
     # classify
     # https://src.fedoraproject.org/rpms/chromium/tree/rawhide
-    if path in [
-        "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/strings/safe_sprintf.h",
-        "third_party/blink/renderer/core/paint/fragment_data_iterator.h",
-    ]:
-        # https://src.fedoraproject.org/rpms/chromium/blob/rawhide/f/chromium-121-nullptr_t-without-namespace-std.patch
-        clazz = "1000-Fedora-chromium-121-nullptr_t-without-namespace-std"
-    elif path in ["build/config/compiler/BUILD.gn"]:
+    if path in ["build/config/compiler/BUILD.gn"]:
         # https://src.fedoraproject.org/rpms/chromium/blob/rawhide/f/chromium-120-split-threshold-for-reg-with-hint.patch
-        clazz = "1001-Fedora-chromium-120-split-threshold-for-reg-with-hint"
-    elif path in [
-        "base/check_op.h",
-        "base/containers/flat_map.h",
-        "chrome/browser/webauthn/authenticator_request_dialog_model.h",
-        "chrome/test/chromedriver/chrome/web_view_impl.cc",
-        "components/feature_engagement/internal/never_event_storage_validator.h",
-        "gin/time_clamper.h",
-        "net/base/net_export.h",
-        "third_party/abseil-cpp/absl/strings/string_view.h",
-        "third_party/dawn/src/tint/lang/spirv/reader/ast_parser/namer.h",
-        "third_party/material_color_utilities/src/cpp/palettes/tones.cc",
-        "third_party/ruy/src/ruy/profiler/instrumentation.h",
-        "third_party/swiftshader/third_party/llvm-10.0/llvm/lib/Support/Unix/Signals.inc",
-        "third_party/vulkan-deps/vulkan-validation-layers/src/layers/external/vma/vk_mem_alloc.h",
-        "third_party/webrtc/audio/utility/channel_mixer.cc",
-        "third_party/webrtc/modules/include/module_common_types_public.h",
-        "ui/gfx/linux/drm_util_linux.h",
-    ]:
-        clazz = "1002-Fedora-chromium-122-missing-header-files"
+        clazz = "1000-Fedora-chromium-120-split-threshold-for-reg-with-hint"
     elif path in ["optional"]:
         # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/absl-optional.patch?ref_type=heads
         clazz = "2000-Debian-fixes-absl-optional"
@@ -74,36 +49,56 @@ for patch in sorted(patches):
         # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc3.patch
         clazz = "2002-Debian-fixes-blink"
     elif path in [
-        "media/filters/temporal_scalability_id_extractor.cc"
+        "chrome/browser/devtools/BUILD.gn"
     ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/extractor-bitset.patch
-        clazz = "2003-Debian-upstream-extractor-bitset"
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/ninja.patch
+        clazz = "2003-Debian-upstream-ninja"
     elif path in [
-        "base/containers/span_reader.h"
+        "chrome/browser/ui/tabs/tab_strip_model.h"
     ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/span-optional.patch
-        clazz = "2004-Debian-upstream-span-optional"
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/tabstrip-include.patch
+        clazz = "2004-Debian-upstream-tabstrip-include"
     elif path in [
-        "components/ukm/bitset.h",
-        "components/ukm/bitset.cc"
+        "components/services/app_service/public/cpp/app_types.h",
     ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/uint-includes.patch
-        clazz = "2005-Debian-upstream-uint-includes"
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/appservice-include.patch
+        clazz = "2005-Debian-upstream-appservice-include"
     elif path in [
-        "content/browser/first_party_sets/first_party_sets_handler_database_helper.h"
+        "chrome/browser/lens/lens_overlay/lens_overlay_url_builder.h"
     ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/fps-optional.patch
-        clazz = "2006-Debian-upstream-fps-optional"
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/lens-include.patch
+        clazz = "2006-Debian-upstream-lens-optional"
     elif path in [
-        "third_party/blink/renderer/bindings/core/v8/script_streamer.cc"
+        "mojo/public/cpp/bindings/lib/bindings_internal.h"
     ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/atomic.patch
-        clazz = "2007-Debian-upstream-atomic"
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/mojo-bindings-include.patch
+        clazz = "2007-Debian-upstream-mojo-bindings-include"
     elif path in [
-        "third_party/blink/renderer/modules/webgpu/gpu_adapter_info.h"
+        "chrome/browser/sync/test/integration/product_specifications_helper.cc",
+        "components/commerce/core/product_specifications/product_specifications_service.cc",
+        "components/commerce/core/product_specifications/product_specifications_service.h",
+        "components/commerce/core/product_specifications/product_specifications_service_unittest.cc",
+        "components/commerce/core/product_specifications/product_specifications_set.cc",
+        "components/commerce/core/product_specifications/product_specifications_set.h",
+        "components/commerce/core/product_specifications/product_specifications_sync_bridge.cc",
+        "components/commerce/core/product_specifications/product_specifications_sync_bridge.h",
+        "components/commerce/core/product_specifications/product_specifications_sync_bridge_unittest.cc",
+        "components/commerce/core/shopping_service.cc",
+        "components/commerce/core/shopping_service.h",
+        "mojo/public/cpp/base/proto_wrapper.cc"
     ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/webgpu-optional.patch
-        clazz = "2008-Debian-upstream-webgpu-optional"
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/no-vector-consts.patch
+        clazz = "2008-Debian-upstream-no-vector-consts"
+    elif path in [
+        "third_party/ruy/src/ruy/profiler/instrumentation.h"
+    ]:
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/ruy-include.patch
+        clazz = "2009-Debian-upstream-ruy-include"
+    elif path in [
+        "third_party/vulkan-deps/vulkan-utility-libraries/src/include/vulkan/utility/vk_small_containers.hpp"
+    ]:
+        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/vulkan-include.patch
+        clazz = "2010-Debian-upstream-vulkan-include"
     elif path in ["tools/v8_context_snapshot/BUILD.gn", "chrome/BUILD.gn"]:
         # https://issues.chromium.org/issues/40945821
         clazz = "3001-rust-ld-bfd"
