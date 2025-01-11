55d3d41a8a8e2963a89acbd1e20ff01ad8a9d2ed
Jiajie Chen <c@jia.je>
2025-01-11 12:17:05 +0800

    Initial rebase on chromium 132.0.6834.83

--- chromium/gen.sh
+++ chromium/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pushd ../../chromium-131.0.6778.85
-diff '--color=auto' -p -X ../chromium-loongarch64/chromium/exclude -N -u -r a b > ../chromium-loongarch64/chromium/chromium-131.0.6778.85.diff
+pushd ../../chromium-132.0.6834.83
+diff '--color=auto' -p -X ../chromium-loongarch64/chromium/exclude -N -u -r a b > ../chromium-loongarch64/chromium/chromium-132.0.6834.83.diff
 popd
 bash strip-datetime.sh
--- chromium/split.py
+++ chromium/split.py
@@ -1,4 +1,4 @@
-version = "131.0.6778.85"
+version = "132.0.6834.83"
 patches = []
 with open(f"chromium-{version}.diff", "r") as f:
     cur = []
@@ -20,128 +20,12 @@ for patch in sorted(patches):
     if path in ["components/media_router/common/providers/cast/channel/enum_table.h"]:
         # https://src.fedoraproject.org/rpms/chromium/blob/rawhide/f/chromium-130-hardware_destructive_interference_size.patch
         clazz = "1000-Fedora-chromium-130-hardware_destructive_interference_size"
-    elif path in ["optional"]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/absl-optional.patch?ref_type=heads
-        clazz = "2000-Debian-fixes-absl-optional"
-    elif path in [
-        "base/allocator/partition_allocator/src/partition_alloc/starscan/stats_collector.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/stats-collector.patch
-        clazz = "2001-Debian-fixes-stats-collector"
-    elif (
-        path.startswith("third_party/blink/renderer/")
-        or path in ["tools/privacy_budget/font_indexer/font_indexer.cc"]
-    ) and path not in [
-        "third_party/blink/renderer/platform/BUILD.gn",
-        "third_party/blink/renderer/platform/graphics/gpu/webgl_image_conversion.cc",
-        "third_party/blink/renderer/platform/graphics/cpu/loongarch64/webgl_image_conversion_lsx.h",
-        "third_party/blink/renderer/bindings/core/v8/script_streamer.cc",
-        "third_party/blink/renderer/modules/webgpu/gpu_adapter_info.h",
-        "third_party/blink/renderer/core/paint/fragment_data_iterator.h",
-        "third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h",
-        "third_party/blink/renderer/modules/encoding/text_decoder.h"
-    ]:
-        # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1067886
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc0000.patch
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc000.patch
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc00.patch
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc0.patch
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc1.patch
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc11.patch
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc2.patch
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/bad-font-gc3.patch
-        clazz = "2002-Debian-fixes-blink"
-    elif path in [
-        "net/third_party/quiche/src/quiche/quic/core/quic_interval_deque.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/quiche-deque.patch
-        clazz = "2003-Debian-fixes-quiche-deque"
-    elif path in [
-        "chrome/browser/ui/tabs/tab_strip_model.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/tabstrip-include.patch
-        clazz = "2004-Debian-upstream-tabstrip-include"
-    elif path in [
-        "gpu/config/webgpu_blocklist_impl.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/gpu-header.patch
-        clazz = "2005-Debian-upstream-gpu-header"
-    elif path in [
-        "third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/blink-header.patch
-        clazz = "2006-Debian-upstream-blink-header"
-    elif path in [
-        "third_party/blink/renderer/modules/encoding/text_decoder.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/blink-header3.patch
-        clazz = "2007-Debian-upstream-blink-header3"
-    elif path in [
-        "content/services/auction_worklet/real_time_reporting_bindings.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/realtime-reporting.patch
-        clazz = "2008-Debian-upstream-realtime-reporting"
-    elif path in [
-        "third_party/ruy/src/ruy/profiler/instrumentation.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/ruy-include.patch
-        clazz = "2009-Debian-upstream-ruy-include"
-    elif path in [
-        "components/visited_url_ranking/public/url_visit.h"
-    ]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/urlvisit-header.patch
-        clazz = "2010-Debian-upstream-urlvisit-header"
-    elif path in ["third_party/blink/renderer/core/paint/fragment_data_iterator.h"]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/fixes/blink-frags.patch
-        clazz = "2011-Debian-fixes-blink-frags"
-    elif path in ["chrome/renderer/accessibility/read_anything_app_model.cc"]:
-        # https://salsa.debian.org/chromium-team/chromium/-/blob/master/debian/patches/upstream/accessibility-format.patch
-        clazz = "2012-Debian-upstream-accessibility-format"
-    elif path in ["tools/v8_context_snapshot/BUILD.gn", "chrome/BUILD.gn"]:
-        # https://issues.chromium.org/issues/40945821
-        clazz = "3001-rust-ld-bfd"
-    elif path in ["third_party/devtools-frontend/src/scripts/build/rollup.config.js"]:
-        clazz = "3002-rollup"
     elif path in ["build/nocompile.gni", "build/rust/rust_bindgen.gni"]:
         clazz = "3003-fix-invalid-substition-type"
     elif path in [
         "build/config/clang/BUILD.gn",
     ]:
         clazz = "3004-fix-clang-builtins-path"
-    elif path in [
-        "third_party/blink/renderer/core/layout/hit_test_request.h",
-        "third_party/blink/renderer/platform/peerconnection/resolution_monitor.cc",
-        "third_party/tflite/src/tensorflow/lite/kernels/internal/spectrogram.h",
-        "third_party/webrtc/common_video/h264/sps_parser.h",
-        "third_party/webrtc/modules/video_coding/utility/ivf_file_reader.cc",
-    ]:
-        clazz = "3005-fix-missing-header"
-    elif path in [
-        "chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.h",
-    ]:
-        clazz = "3006-fix-static-assertion"
-    elif path in [
-        "third_party/angle/src/libANGLE/renderer/vulkan/FramebufferVk.cpp",
-    ]:
-        # https://issues.chromium.org/issues/41455655#comment473
-        # https://chromium.googlesource.com/angle/angle.git/+/2f934a47e9709cac9ce04d312b7aa496948bced6%5E%21/#F0
-        clazz = "3007-replace-powf-with-pow"
-    elif path in [
-        "content/browser/BUILD.gn",
-        "chrome/browser/safe_browsing/BUILD.gn"
-    ]:
-        # https://bugs.gentoo.org/930112
-        # https://bugs.gentoo.org/930107
-        # https://gitweb.gentoo.org/repo/gentoo.git/tree/dev-qt/qtwebengine/files/qtwebengine-6.7.0-ninja1.12.patch
-        clazz = "3008-fix-ninja-race-condition"
-    elif path in [
-        "third_party/libvpx/BUILD.gn",
-        "third_party/blink/renderer/platform/BUILD.gn",
-        "third_party/blink/renderer/platform/graphics/gpu/webgl_image_conversion.cc",
-        "third_party/libvpx/source/config/linux/loongarch/vpx_dsp_rtcd.h",
-        "third_party/libvpx/source/config/linux/loongarch/vp8_rtcd.h",
-    ]:
-        clazz = "4000-loongarch64-clang-no-lsx"
     elif path.startswith("third_party/swiftshader/"):
         clazz = "4001-loongarch64-swiftshader"
     elif path.startswith("sandbox/"):
