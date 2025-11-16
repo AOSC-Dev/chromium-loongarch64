c41f548c9ab45944ec89d85921edb974c8f0e400
darkyzhou <me@zqy.io>
2025-11-16 12:39:54 +0800

    chromium: rebase against 142.0.7444.134

--- chromium/gen.sh
+++ chromium/gen.sh
@@ -3,5 +3,5 @@ VERSION=$(cat VERSION)
 pushd ../../chromium-${VERSION}
 diff '--color=auto' -p -X ../chromium-loongarch64/chromium/exclude -N -u -r a b > ../chromium-loongarch64/chromium/chromium-${VERSION}.diff
 popd
-bash strip-datetime.sh
+python3 format.py chromium-${VERSION}.diff
 python3 split.py
